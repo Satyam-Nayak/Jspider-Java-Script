5517d3692ad42cf2e2b20d21b2478a5b6139705c
Satyam-Nayak <satyamn120@gmail.com>
2024-08-14 15:43:39 +0530

    14-08-2024 2nd class java script

---
+++ Java Script/14-08-2024 2nd class/js1.js
@@ -0,0 +1,17 @@
+var a; // declare
+a=10;  // initialise
+a="hi"; // re- initialise
+var a =true; // re- declare
+// console.log("Value of the container is : " +a);
+
+let b; // declare
+b=20; // initialise
+b=30; // re- initialise
+// let b ="Hi";  // can not re-declare the value
+// console.log("value of the container: "+b);
+
+
+const c =100;  // declare and initialise
+// const c = 200;  // can not re-declare
+// c=true; // can not re-initialise
+console.log("value of the container: "+c);
