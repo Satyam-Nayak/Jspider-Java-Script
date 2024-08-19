96a8c37f1678da0e5918570ca00e6bc675e24259
Satyam-Nayak <satyamn120@gmail.com>
2024-08-19 15:43:04 +0530

    19-08-2024 Harish sir 4th class JavaScript

---
+++ Java Script/19-08-2024 4th class functions/jsq1.js
@@ -0,0 +1,23 @@
+function print(){
+    console.log("Welcome to BTM");
+
+}
+
+print();
+print();
+
+function add(a,b){
+    console.log(a+b);
+}
+add(true,10);
+//add(10);
+//add(10,20,30);
+//add("hi",10,69);
+
+console.log(true,true);
+console.log(true,false);
+console.log(false,false);
+console.log(false);
+console.log(true);
+console.log("10",10);
+add(10);
