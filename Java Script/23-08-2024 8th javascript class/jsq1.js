2524c61f71106c1529d820a3f54eefa1f14bda6f
Satyam-Nayak <satyamn120@gmail.com>
2024-08-23 22:27:59 +0530

    23-08-2024bjavascript harish

---
+++ Java Script/23-08-2024 8th javascript class/jsq1.js
@@ -0,0 +1,10 @@
+let arr1 = [10,"hi","true",null,[true,21,"Bangalore"]];
+console.log(arr1);
+console.log(arr1.length);
+console.log(arr1[0]);
+console.log(arr1[2]);
+console.log(arr1[4]);
+console.log(arr1[4][0]);
+console.log(arr1[4][2]);
+console.log(arr1[arr1.length+2]);
+// will have there index position starting from 0
---
+++ Java Script/23-08-2024 8th javascript class/jsq2.js
@@ -0,0 +1,18 @@
+let arr1 = [10,true,'hi',false];
+console.log(arr1);
+
+//push
+arr1.push("hello",123,null);
+console.log(arr1);
+
+//pop
+arr1.pop();
+console.log(arr1);
+
+//unshift
+arr1.unshift("Hello",132,null);
+console.log(arr1);
+
+//shift
+arr1.shift();
+console.log(arr1);
