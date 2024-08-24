2ace23b437b88322cad98eb7996698b640617a79
Satyam-Nayak <satyamn120@gmail.com>
2024-08-24 13:53:53 +0530

    24-08-2024 Harish sir

---
+++ Java Script/24-08-2024 9th class/jsq1.js
@@ -0,0 +1,34 @@
+let arr1 = [10,true,'hi',false,true];
+console.log(arr1);
+
+// let str = arr1.join("-")
+// console.log(str);
+
+// let str = arr1.toString();
+// console.log(str);
+
+// arr1.reverse();
+// console.log(arr1);
+
+// let newArr = arr1.slice(1,3);
+// console.log(newArr);
+
+// arr1.splice(2,1,"one","true","JSP",23,true);
+// arr1.splice(1,0);  //start from 1 but not delete anything
+// console.log(arr1);
+
+let bool = arr1.includes('hi'); // which one present in the array is shown true
+let bool1 = arr1.includes(true,2); // which one present in the array is shown true
+console.log(bool1);
+
+let arr2 = [10,true,'hi',false,"hi","hi"];
+console.log(arr2);
+
+let num = arr2.indexOf("hi",3);
+console.log(num);
+
+let num1 = arr1.lastIndexOf("hi");
+console.log(num1);
+
+let bool3 = arr2. includes(true,2);
+console.log(bool3)
