3dd9e92fa5a81350a30f5a6d30114022b278de85
Satyam-Nayak <satyamn120@gmail.com>
2024-09-10 16:08:44 +0530

    dom 10-09-2024 1st class

---
+++ Dom/10-09-2024 Dom 1st class/dom.js
@@ -0,0 +1,22 @@
+let a1 = document.createElement("h1");
+console.log(a1);
+a1.innerText = "Hello Guys........";
+a1.style.backgroundColor = "green"
+a1.style.color = "white"
+
+let body = document.getElementById('m1');
+console.log(body);
+
+let image = document.createElement('img');
+console.log(image);
+image.src = "https://images.alphacoders.com/850/850903.png"
+
+let butt = document.createElement('button');
+console.log(butt);
+butt.innerText = "click me..";
+butt.style.backgroundColor = "black"
+butt.style.color = "white"
+
+body.appendChild(a1);
+body.append(image);
+body.append(butt);
