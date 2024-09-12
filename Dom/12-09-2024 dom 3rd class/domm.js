75542cde170a6531a052efdca176143cf70a7ec1
Satyam-Nayak <satyamn120@gmail.com>
2024-09-12 15:56:21 +0530

    12-09-2024 dom screenn

---
+++ Dom/12-09-2024 dom 3rd class/domm.js
@@ -0,0 +1,14 @@
+let c1 = document.getElementsByTagName('b');
+console.log(c1);
+c1[0].innerText = "I am Satyam"
+c1[1].innerText = "I am sonu"
+
+let d1 = document.querySelector('#abc');
+console.log(d1);
+
+let m1= document.querySelector(".monga");
+console.log(m1);
+
+let b1 = document.querySelectorAll("b");
+console.log(b1);
+b1[0].innerText = "i am vina"
